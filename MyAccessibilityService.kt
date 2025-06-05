e49628914ec015bb2c8f376b605c462235424303
Ayham19877 <turky.jadou@gmail.com>
2025-06-05 12:08:56 -0500

    Add files via upload

---
+++ MyAccessibilityService.kt
@@ -0,0 +1,9 @@
+package com.example.homsapp
+
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class MyAccessibilityService : AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+    override fun onInterrupt() {}
+}
