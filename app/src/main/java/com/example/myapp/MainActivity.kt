1c3c499bfadf57b582ba422eb1699d154fc81e6a
Ayham19877 <turky.jadou@gmail.com>
2025-06-05 14:46:11 -0600

    Add files via upload

--- app/src/main/java/com/example/myapp/MainActivity.kt
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -1,3 +1,11 @@
+
 package com.example.myapp
 
-class MainActivity {}
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
--- app/src/main/java/com/example/myapp/MyAccessibilityService.kt
+++ app/src/main/java/com/example/myapp/MyAccessibilityService.kt
@@ -1,3 +1,10 @@
+
 package com.example.myapp
 
-class MyAccessibilityService {}
+import android.accessibilityservice.AccessibilityService
+import android.view.accessibility.AccessibilityEvent
+
+class MyAccessibilityService : AccessibilityService() {
+    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+    override fun onInterrupt() {}
+}
